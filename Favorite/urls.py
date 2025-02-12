3304895b4377380e8af586737a0f6a754d735df6
lucifervenom00 <unique.myself10402906@gmail.com>
2025-02-12 21:54:52 +0545

    Fixed Favorites page design and its working.

--- Favorite/urls.py
+++ Favorite/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns=[
     path('favorites/',views.favorites,name='favorite'),
     path('add-to-favorites/<int:product_id>/',views.add_items_on_favorite,name='add-to-favorite'),
+    path('remove-from-favorites/<int:product_id>/<int:item_id>/',views.remove_items_from_favorites,name='remove-from-favorites'),
 ]
--- Favorite/views.py
+++ Favorite/views.py
@@ -4,12 +4,12 @@ from Main.models import ProductVariant
 from .models import Favorite,FavoriteItem
 # Create your views here.
 def favorites(request):
-    favorite=Favorite.objects.get(user=request.user)   
-    
+    favorite=Favorite.objects.get(user=request.user)  
     items = favorite.favoriteitem_set.all()
     context={
-        "product":items
-     }
+            "product":items,
+            "favorites":favorite
+        }
     return render(request,"favorites/favorite_list.html",context)
         
 
@@ -23,3 +23,12 @@ def add_items_on_favorite(request,product_id):
 
     return redirect("favorite")
 
+@login_required
+def remove_items_from_favorites(request,product_id,item_id):
+    product=ProductVariant.objects.get(id=product_id)
+    
+    # item=FavoriteItem.objects.get(id=item_id)
+
+    item=FavoriteItem.objects.get(id=item_id,item=product).delete()
+    
+    return redirect ("favorite")
